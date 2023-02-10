5df1eb3d89f0ed8f23d00a972f53d3f9ece0513f
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-02-10 18:20:05 +0100

    some bugs fixed

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ const places = document.querySelectorAll(".tiktoe__place");
 
 let placesCheckArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-let shape = "X";
+let shape = "O";
 let GameEnd = false;
 
 
@@ -52,18 +52,62 @@ const checkWin =(shape,placesCheckArray,index)=>{
         if(placesCheckArray[index-4]==shape &&placesCheckArray[index-8]==shape){
             return  true;
         }
+    }if(index==2){
+        if(placesCheckArray[index+2]==shape &&placesCheckArray[index+4]==shape){
+            return  true;
+        }
+    }
+    else if(index==4){
+        if(placesCheckArray[index+2]==shape &&placesCheckArray[index-2]==shape){
+            return  true;
+        }
+    }else if(index==6){
+        console.log("piwo");
+        if(placesCheckArray[index-2]==shape &&placesCheckArray[index-4]==shape){
+            return  true;
+        }
     }
 };
+
+const draw =(placesCheckArray)=>{
+    for (let i = 0; i < 9; i++) {
+        if(placesCheckArray[i]==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+const resetGame = (placesCheckArray,places)=>{
+    places.forEach((place)=>{
+        place.innerText="";
+    })
+    for(let i=0;i<9;i++){
+        placesCheckArray[i]=0;
+    }
+    document.querySelector(".game__end").innerText="";
+    shape = "O";
+    GameEnd=false;
+}
+
+const gameReset = document.querySelector(".js-game__reset");
+gameReset.addEventListener("click",()=>{
+    resetGame(placesCheckArray,places);
+});
 
 places.forEach((place, index) => {
   place.addEventListener("click", () => {
     if (place.innerText === "" && !GameEnd) {
       place.innerText = shape;
       placesCheckArray[index] = shape;
+      if(draw(placesCheckArray)){
+        document.querySelector(".game__end").innerText= "DRAW";
+      }
       if(checkWin(shape,placesCheckArray,index)){
         document.querySelector(".game__end").innerText= `${shape} WIN`;
         GameEnd=true;
       }
+     
       shape = toggleShape(shape);
     }
   });
